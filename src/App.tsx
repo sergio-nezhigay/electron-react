d634a54b9739f160dab646fc3c72241feb0f69af
Sergio Nezhigay <abc1971h@gmail.com>
2025-03-06 11:37:00 +0200

    initialize Electron app with React, including main and renderer processes, basic HTML structure, and ESLint configuration

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+import Home from './page';
+
+const root = createRoot(document.body);
+
+root.render(<Home />);
---
+++ src/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = React.memo((props: any) => {
+  return <div>Hello world1</div>;
+});
+
+export default Home;
